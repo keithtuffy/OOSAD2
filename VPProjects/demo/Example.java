2c47e59db8f5509c156f019f86b04ea91610262a
keithtuffy <keithtuffy@live.com>
2015-02-05 11:07:46 +0000

    OOsad week2

---
+++ VPProjects/demo/Example.java
@@ -0,0 +1,37 @@
+package demo;
+
+public class Example {
+
+	private string name;
+	private int age;
+
+	public Example() {
+		// TODO - implement Example.Example
+		throw new UnsupportedOperationException();
+	}
+
+	public string getName() {
+		return this.name;
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(string name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	/**
+	 * 
+	 * @param age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
